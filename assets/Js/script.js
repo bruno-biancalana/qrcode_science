48e685165572b5bd52dc1824897b03e536edbd8a
Bruno <bruno.biancalana@outlook.com>
2022-11-23 10:51:34 -0300

    adiciona pop up na tela do formulário

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,3 +1,25 @@
+function mensagem(){
+Swal.fire({
+  title: '<strong>A science Valley <u>não</u> compartilhará suas informações com terceiros</strong>',
+  icon: 'info',
+  html:
+    'Acesse a política de armazenamento e privacidade de dados neste ' +
+    '<a href="https://svriglobal.com/wp-content/uploads/2022/07/Politica-de-Privacidade-e-Cookies-SVRI.pdf">link.</a> ',
+  showCloseButton: true,
+  showCancelButton: true,
+  focusConfirm: false,
+  confirmButtonText:
+    '<i class="fa fa-thumbs-up"></i> Great!',
+  confirmButtonAriaLabel: 'Thumbs up, great!',
+  cancelButtonText:
+    '<i class="fa fa-thumbs-down"></i>',
+  cancelButtonAriaLabel: 'Thumbs down'
+})}
+
+
+
+
+//Alert após envio de dados->
 function botao(){
   Swal.fire({
     position: 'top-center',
@@ -6,3 +28,4 @@ function botao(){
     showConfirmButton: false,
     timer: 3000
   })}
+  //Final do alert->
