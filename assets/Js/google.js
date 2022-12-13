aeef6d75d9f40af0f735594ac6e3f0477dccab9c
Bruno <bruno.biancalana@outlook.com>
2022-12-13 10:02:30 -0300

    teste

--- assets/Js/google.js
+++ assets/Js/google.js
@@ -1,12 +1,4 @@
-<script type="module" src="js/jwt-decode.min.js"></script>
-
-import jwt_decode from "jwt-decode";
 
 
-function decode(){
-var userObject = jwt_decode(response.credential);
-console.log(userObject);
-}
 
-export default decode;
 
